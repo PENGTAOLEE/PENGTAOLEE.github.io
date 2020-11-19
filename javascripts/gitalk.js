2c8e1bfeedf0090f9998ac094acf7a784dceee81
lpt <859720798@qq.com>
2020-11-19 12:36:39 +0800

    Site updated: 2020-11-19 12:36:39

--- javascripts/gitalk.js
+++ javascripts/gitalk.js
@@ -1,7 +1,11 @@
 ;(function () {
     if (window.aomori && window.aomori.gitalk) {
         window.aomori.gitalk.id = md5(window.location.href)
-        const gitalk = new Gitalk(window.aomori.gitalk)
-        gitalk.render('gitalk-container')
+        const gitalkDom = document.getElementById('gitalk-container')
+        if (gitalkDom) {
+            const gitalk = new Gitalk(window.aomori.gitalk)
+            gitalk.render('gitalk-container')
+        }
+        
     }
 })()
