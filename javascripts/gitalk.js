7a4d57463fc2c8770ad10e6f5fc1b4f853a95955
lpt <859720798@qq.com>
2020-11-19 12:43:41 +0800

    Site updated: 2020-11-19 12:43:41

--- javascripts/gitalk.js
+++ javascripts/gitalk.js
@@ -1,11 +1,7 @@
 ;(function () {
     if (window.aomori && window.aomori.gitalk) {
         window.aomori.gitalk.id = md5(window.location.href)
-        const gitalkDom = document.getElementById('gitalk-container')
-        if (gitalkDom.length) {
-            const gitalk = new Gitalk(window.aomori.gitalk)
-            gitalk.render('gitalk-container')
-        }
-        
+        const gitalk = new Gitalk(window.aomori.gitalk)
+        gitalk.render('gitalk-container')
     }
 })()
