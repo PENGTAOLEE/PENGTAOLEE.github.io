26037185b40d7e6812f39924e63636a06d184feb
lpt <859720798@qq.com>
2020-11-19 12:38:00 +0800

    Site updated: 2020-11-19 12:38:00

--- javascripts/gitalk.js
+++ javascripts/gitalk.js
@@ -2,7 +2,7 @@
     if (window.aomori && window.aomori.gitalk) {
         window.aomori.gitalk.id = md5(window.location.href)
         const gitalkDom = document.getElementById('gitalk-container')
-        if (gitalkDom) {
+        if (gitalkDom.length) {
             const gitalk = new Gitalk(window.aomori.gitalk)
             gitalk.render('gitalk-container')
         }
